25431e062b151b38ba059a32fc653f80238abd4d
pmckeegan <pmckeegan@gmail.com>
2019-03-12 23:54:56 +0000

    added loop functionality properly

--- express_server.js
+++ express_server.js
@@ -27,7 +27,7 @@ app.get("/hello", (req, res) =>{
 })
 
 app.get("/urls", (req, res) => {
-  let templateVars = {urls: urlDatabase };
+  let templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
