c321d339486abb37715ece2ddb8cfc7284a9ada9
pmckeegan <pmckeegan@gmail.com>
2019-03-12 17:53:19 +0000

    added barebones url functionality

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ var PORT = 8080 //default port 8080
 
 app.set("view engine", "ejs");
 
+
 var urlDatabase = {
   "b2xvn2": "http://www.lighthouselabs.ca",
   "9sd5xk": "http://www.google.com"
@@ -24,3 +25,8 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) =>{
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 })
+
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
