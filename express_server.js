96795929b69cc5dae87dc8827d0ab83062b47a89
pmckeegan <pmckeegan@gmail.com>
2019-03-12 16:16:51 +0000

    created the express_server.js

---
+++ express_server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var PORT = 8080 //default port 8080
+
+var urlDatabase = {
+  "b2xvn2": "http://www.lighthouselabs.ca",
+  "9sd5xk": "http://www.google.com"
+};
+
+app.get("/" , (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example App listening on ${PORT}!`);
+});
